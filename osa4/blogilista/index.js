ec7b96c7b8aa9b1beb4d635aa60667d2b84542cd
Samuel0c <sleppiniemi@protonmail.com>
2018-02-14 15:05:11 +0200

    osa4 teht2

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -4,42 +4,18 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = mongoose.model('Blog', {
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-module.exports = Blog
+const middleware = require('./utils/middleware')
+const blogsRouter = require('./controllers/blogs')
+const blogModel = require('./models/blog')
 
 app.use(cors())
 app.use(bodyParser.json())
+app.use(middleware.logger)
+app.use('/api/blogs', blogsRouter)
+app.use(middleware.error)
 
-const mongoUrl = 'mongodb://samuel:salasana@ds235827.mlab.com:35827/blogit'
+const mongoUrl = 'mongodb://samuel:passwordHere@ds235827.mlab.com:35827/blogit'
 mongoose.connect(mongoUrl)
 mongoose.Promise = global.Promise
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(process.env.PORT || 3003)
