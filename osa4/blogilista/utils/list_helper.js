b73f0462d0c6e86b8c35c6377c16d0ec29dbed25
Samuel0c <sleppiniemi@protonmail.com>
2018-02-14 15:37:06 +0200

    osa4 teht4 ekoja testejä

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (list) => {
+  const reducer = (sum, blog) => {
+    return sum + blog.likes
+  }
+  return list.length === 0 ? 0 : list.reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
