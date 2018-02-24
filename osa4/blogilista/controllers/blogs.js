0c7d07fcc5693ca6fb1777dc25e67b1f2730c74c
Samuel0c <sleppiniemi@protonmail.com>
2018-02-24 20:59:52 +0200

    osa4 teht 12-14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -4,7 +4,50 @@ const Blog = require('../models/blog')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
-  response.json(blogs.map(Blog))
+  response.json(blogs)
+})
+
+/* blogsRouter.get('/:id', async (request, response) => {
+  try {
+    const blog = await Blog.findById(request.params.id)
+
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+
+  } catch (exception) {
+    console.log(exception)
+    response.status(400).send({ error: 'malformatted id' })
+  }
+}) */
+
+blogsRouter.delete('/:id', async (request, response) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+
+    response.status(204).end()
+  } catch (exception) {
+    response.status(400).send({ error: 'malformatted id' })
+  }
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const object = {
+    title: request.body.title,
+    author: request.body.author,
+    url: request.body.url,
+    likes: request.body.likes || 0
+  }
+
+  try {
+    await Blog.findByIdAndUpdate(request.params.id, object, { new: true })
+
+    response.status(200).end()
+  } catch (exception) {
+    response.status(400).send({ error: 'malformatted id' })
+  }
 })
 
 
@@ -15,11 +58,11 @@ blogsRouter.post('/', (request, response) => {
     url: request.body.url,
     likes: request.body.likes || 0
   }
-  const blog = new Blog(object)
-
-  if (!blog.title || !blog.url) {
+  
+  if (!object.title || !object.url) {
     return response.status(400).end()
   }
+  const blog = new Blog(object)
 
   blog
     .save()
--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -2,35 +2,18 @@ const supertest = require('supertest')
 const { app, server } = require('../index')
 const api = supertest(app)
 const Blog = require('../models/blog')
-
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  }
-]
+const helper = require('./test_helper')
 
 beforeAll(async () => {
   await Blog.remove({})
 
-  const blogObjects = initialBlogs.map(blog => new Blog(blog))
+  const blogObjects = helper.initialBlogs.map(blog => new Blog(blog))
   const promiseArray = blogObjects.map(blog => blog.save())
   await Promise.all(promiseArray)
 })
 
 test('blogs are returned as json', async () => {
+
   await api
     .get('/api/blogs')
     .expect(200)
@@ -38,24 +21,20 @@ test('blogs are returned as json', async () => {
 })
 
 test('all blogs are returned', async () => {
-  const response = await api
-    .get('/api/blogs')
+  const blogs = await helper.blogsInDb()
+  const initBlogs = helper.initialBlogs
 
-  expect(response.body.length).toBe(initialBlogs.length)
+  expect(blogs.length).toBe(initBlogs.length)
 })
 
 test('a specific blog is within the returned notes', async () => {
-  const response = await api
-    .get('/api/blogs')
+  const blogs = await helper.blogsInDb()
 
-  const titles = response.body.map(r => r.title)
-
-  expect(titles).toContain('Go To Statement Considered Harmful')
+  expect(blogs.map(t => t.title)).toContain('Go To Statement Considered Harmful')
 })
 
 test('a valid blog can be added ', async () => {
   const newBlog = {
-    _id: "5a422b3a1b54a676234d17f9",
     title: "Canonical string reduction",
     author: "Edsger W. Dijkstra",
     url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
@@ -63,24 +42,22 @@ test('a valid blog can be added ', async () => {
     __v: 0
   }
 
+  const blogsBefore = await helper.blogsInDb()
+
   await api
     .post('/api/blogs')
     .send(newBlog)
-    .expect(201)  //or should this be 200?
+    .expect(201)
     .expect('Content-Type', /application\/json/)
 
-  const response = await api
-    .get('/api/blogs')
+  const blogsAfter = await helper.blogsInDb()
 
-  const titles = response.body.map(r => r.title)
-
-  expect(response.body.length).toBe(initialBlogs.length + 1)
-  expect(titles).toContain('Canonical string reduction')
+  expect(blogsAfter.length).toBe(blogsBefore.length + 1)
+  expect(blogsAfter.map(b => b.title)).toContain(newBlog.title)
 })
 
 test('a blog with undefined likes has defaults zero likes ', async () => {
   const newBlog = {
-    _id: "5a422b891b54a676234d17fa",
     title: "First class tests",
     author: "Robert C. Martin",
     url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
@@ -103,7 +80,6 @@ test('a blog with undefined likes has defaults zero likes ', async () => {
 
 test('a blog without title cannot be added ', async () => {
   const newBlog = {
-    _id: "5a422ba71b54a676234d17fb",
     author: "Jiipu Ivaneva",
     url: "http://tajukankaankutoja.sarjakuvablogit.com/",
     likes: 10,
@@ -118,7 +94,6 @@ test('a blog without title cannot be added ', async () => {
 
 test('a blog without url cannot be added ', async () => {
   const newBlog = {
-    _id: "5a422ba71b54a676234d17fj",
     title: "Susi sorakuopassa",
     author: "Wolf Kankare",
     likes: 9,
@@ -129,6 +104,62 @@ test('a blog without url cannot be added ', async () => {
     .post('/api/blogs')
     .send(newBlog)
     .expect(400)
+})
+
+test('a blog can be deleted', async () => {
+  const newBlog = {
+    title: "Chocochili",
+    author: "Elina Innanen",
+    url:"https://chocochili.net/",
+    likes: 9,
+    __v: 0
+  }
+
+  const addedBlog = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  const blogsBefore = await helper.blogsInDb()  
+
+  await api
+    .delete(`/api/blogs/${addedBlog.body._id}`)
+    .expect(204)
+
+  const blogsAfter = await helper.blogsInDb()
+
+  expect(blogsAfter.map(b => b.title)).not.toContain('Chocochili')
+  expect(blogsAfter.length).toBe(blogsBefore.length - 1)
+})
+
+test('a blog can be updated', async () => {
+  const newBlog = {
+    title: "Viimeinen muru",
+    author: "Saara",
+    url:"https://www.viimeistamuruamyoten.com/",
+    likes: 8,
+    __v: 0
+  }
+
+  const addedBlog = await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+
+  const blogsBefore = await helper.blogsInDb()  
+
+  newBlog.title = "Viimeistä murua myöten"
+
+  await api
+    .put(`/api/blogs/${addedBlog.body._id}`)
+    .send(newBlog)
+    .expect(200)
+
+  const blogsAfter = await helper.blogsInDb()
+
+  expect(blogsAfter.map(b => b.title)).not.toContain('Viimeinen muru')
+  expect(blogsAfter.map(b => b.title)).toContain('Viimeistä murua myöten')
+  expect(blogsAfter.length).toBe(blogsBefore.length)
 })
 
 afterAll(() => {
