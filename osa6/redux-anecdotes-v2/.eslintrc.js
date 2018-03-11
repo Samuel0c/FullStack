f3360c8e13fa7463610298d7ffc3a8a764b9b82d
Samuel0c <sleppiniemi@protonmail.com>
2018-03-11 16:24:38 +0200

    osa6 teht 1

---
+++ osa6/redux-anecdotes-v2/.eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "experimentalObjectRestSpread": true,
+            "jsx": true
+        },
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "no-console": 0,
+        "react/prop-types": 0,
+        "eqeqeq": "error",
+        "no-trailing-spaces": "error",
+        "object-curly-spacing": [
+            "error", "always"
+        ],
+        "arrow-spacing": [
+            "error", { "before": true, "after": true }
+        ],
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ],
+        "parser": "babel-eslint",
+        "parserOptions": {
+            "sourceType": "module",
+            "ecmaVersion": 6,
+            "ecmaFeatures": {
+                "experimentalObjectRestSpread": true
+            }
+        }
+    }
+};
---
+++ osa6/redux-anecdotes-v2/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import store from './store'
+import anecdoteReducer from './reducers/anecdoteReducer'
+
+
+const render = () => {
+  ReactDOM.render(
+    <App store={store} />,
+    document.getElementById('root')
+  )
+}
+
+render()
+store.subscribe(render)
